2337761f039a125a549c1527be0ae39ef009e77b
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-03-10 22:08:08 +0000

    Implement svc benchmarking

--- analysis.py
+++ analysis.py
@@ -2,13 +2,13 @@ from datetime import datetime
 from few_shot_predictor import FewShotPredictor
 import logging
 import numpy as np
-from numpy import ndarray
 import pandas as pd
 from pandas import DataFrame
 from pathlib import Path
 from pyrepseq.metric import tcr_metric
 from pyrepseq.metric.tcr_metric import TcrMetric
 import random
+from sceptr import variant
 from sklearn import metrics
 from tqdm import tqdm
 from typing import Dict, Iterable, List
@@ -24,13 +24,14 @@ BENCHMARKS_DIR = PROJECT_ROOT/"benchmarks"
 
 LABELLED_DATA = pd.read_csv("tcr_data/preprocessed/benchmarking/vdjdb_cleaned.csv")
 BACKGROUND_DATA = pd.read_csv("tcr_data/preprocessed/tanno/test.csv")
-BACKGROUND_SAMPLE = BACKGROUND_DATA.sample(n=1000,random_state=420)
+BACKGROUND_SAMPLE = BACKGROUND_DATA.sample(n=100,random_state=420)
 
 EPITOPES = LABELLED_DATA.Epitope.unique()
 
 MODELS = (
     tcr_metric.Cdr3Levenshtein(),
-    # tcr_metric.Tcrdist()
+    tcr_metric.Tcrdist(),
+    variant.ab_sceptr()
 )
 
 
@@ -92,7 +93,7 @@ def get_few_shot_results(model: TcrMetric) -> Dict[str, DataFrame]:
     print(f"Commencing few-shot benchmarking for {model.name}...")
 
     results = dict()
-    for num_shots in (5, 10):
+    for num_shots in (10,100,200):
         k_shot_results = get_k_shot_results(model, k=num_shots)
         results.update(k_shot_results)
     return results
@@ -101,8 +102,8 @@ def get_few_shot_results(model: TcrMetric) -> Dict[str, DataFrame]:
 def get_k_shot_results(model: TcrMetric, k: int) -> Dict[str, DataFrame]:
     nn_results = []
     avg_dist_results = []
-    svc_results = []
-    mlp_results = []
+    # svc_results = []
+    # mlp_results = []
     
     for epitope in tqdm(EPITOPES):
         labelled_data_epitope_mask = LABELLED_DATA.Epitope == epitope
@@ -117,7 +118,7 @@ def get_k_shot_results(model: TcrMetric, k: int) -> Dict[str, DataFrame]:
         ref_index_sets = [
             random.sample(ref_index_sets, k=k) for _ in range(100)
         ]
-        logging.info(f"The first five indices in the first {k}-shot reference set for {epitope} for {model.name} are: {ref_index_sets[0][:5]}")
+        logging.info(f"The first ten indices in the first {k}-shot reference set for {epitope} for {model.name} are: {ref_index_sets[0][:10]}")
 
         auc_summaries = get_k_shot_auc_summaries_for_epitope(model, epitope, ref_index_sets)
 
@@ -125,20 +126,21 @@ def get_k_shot_results(model: TcrMetric, k: int) -> Dict[str, DataFrame]:
         avg_dist_results.append(auc_summaries["avg_dist"])
         # svc_results.append(auc_summaries["svc"])
 
-        if "mlp" in auc_summaries:
-            mlp_results.append(auc_summaries["mlp"])
+        # if "mlp" in auc_summaries:
+        #     mlp_results.append(auc_summaries["mlp"])
 
     return {
         f"{k}_shot_nn": DataFrame.from_records(nn_results),
         f"{k}_shot_avg_dist": DataFrame.from_records(avg_dist_results),
         # f"{k}_shot_svc": DataFrame.from_records(svc_results),
-        f"{k}_shot_mlp": DataFrame.from_records(mlp_results)
+        # f"{k}_shot_mlp": DataFrame.from_records(mlp_results)
     }
 
 
 def get_k_shot_auc_summaries_for_epitope(model: TcrMetric, epitope: str, ref_index_sets: Iterable[List[int]]) -> Dict[str, Dict[str, float]]:
     nn_aucs = []
     avg_dist_aucs = []
+    svc_aucs = []
     
     for ref_index_set in ref_index_sets:
         positive_refs = LABELLED_DATA.loc[ref_index_set]
@@ -148,16 +150,20 @@ def get_k_shot_auc_summaries_for_epitope(model: TcrMetric, epitope: str, ref_ind
 
         nn_scores = predictor.get_nn_inferences(queries)
         avg_dist_scores = predictor.get_avg_dist_inferences(queries)
+        # svc_scores = predictor.get_svc_inferences(queries)
 
         nn_auc = metrics.roc_auc_score(ground_truth, nn_scores)
         avg_dist_auc = metrics.roc_auc_score(ground_truth, avg_dist_scores)
+        # svc_auc = metrics.roc_auc_score(ground_truth, svc_scores)
 
         nn_aucs.append(nn_auc)
         avg_dist_aucs.append(avg_dist_auc)
+        # svc_aucs.append(svc_auc)
     
     return {
         "nn": get_auc_summary(epitope, nn_aucs),
-        "avg_dist": get_auc_summary(epitope, avg_dist_aucs)
+        "avg_dist": get_auc_summary(epitope, avg_dist_aucs),
+        # "svc": get_auc_summary(epitope, svc_aucs)
     }
 
 
--- few_shot_predictor.py
+++ few_shot_predictor.py
@@ -1,6 +1,14 @@
+import numpy as np
 from numpy import ndarray
+import pandas as pd
 from pandas import DataFrame
 from pyrepseq.metric.tcr_metric import TcrMetric
+from sklearn.svm import SVC
+import utils
+
+
+def rbf_kernel(cdist: ndarray, characteristic_length: float = 1) -> ndarray:
+    return np.exp(- (cdist / characteristic_length) ** 2)
 
 
 class FewShotPredictor:
@@ -8,13 +16,30 @@ class FewShotPredictor:
         self._metric = metric
         self._positive_refs = positive_refs
         self._bg_refs = bg_refs
+        self._svc = self._train_svc()
+
+    def _train_svc(self) -> SVC:
+        training_data = pd.concat([self._positive_refs, self._bg_refs])
+        ground_truth = [True] * len(self._positive_refs) + [False] * len(self._bg_refs)
+        raw_cdist_matrix = self._metric.calc_cdist_matrix(training_data, training_data)
+        kernel_cdist_matrix = rbf_kernel(raw_cdist_matrix)
+        
+        svc = SVC(kernel="precomputed", probability=True, class_weight="balanced")
+        svc.fit(kernel_cdist_matrix, ground_truth)
+        return svc
     
     def get_nn_inferences(self, queries: DataFrame) -> ndarray:
         cdist_matrix = self._metric.calc_cdist_matrix(queries, self._positive_refs)
         nn_dists = np.min(cdist_matrix, axis=1)
-        return convert_dists_to_scores(nn_dists)
+        return utils.convert_dists_to_scores(nn_dists)
     
     def get_avg_dist_inferences(self, queries: DataFrame) -> ndarray:
         cdist_matrix = self._metric.calc_cdist_matrix(queries, self._positive_refs)
         avg_dists = np.mean(cdist_matrix, axis=1)
-        return convert_dists_to_scores(avg_dists)
+        return utils.convert_dists_to_scores(avg_dists)
+
+    def get_svc_inferences(self, queries: DataFrame) -> ndarray:
+        training_data = pd.concat([self._positive_refs, self._bg_refs])
+        raw_cdist_matrix = self._metric.calc_cdist_matrix(queries, training_data)
+        kernel_cdist_matrix = rbf_kernel(raw_cdist_matrix)
+        return self._svc.predict_proba(kernel_cdist_matrix)[:,1]

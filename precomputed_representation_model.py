a2e224784ee923119ad356bc3178dee3921d71d3
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-03-21 11:03:25 +0000

    Precomputed wrapper saves cache

--- precomputed_representation_model.py
+++ precomputed_representation_model.py
@@ -3,18 +3,31 @@ from numpy import ndarray
 import pandas as pd
 from pandas import DataFrame, Series
 from pathlib import Path
+import pickle
 from tcr_representation_model import TcrRepresentationModel
 from torch import FloatTensor
 from typing import Dict, Optional, Tuple
 
 
 PROJECT_ROOT = Path(__file__).parent.resolve()
+CACHE_DIR = PROJECT_ROOT/".precomputed_representation_cache"
+CACHE_DIR.mkdir(exist_ok=True)
 
 
 class PrecomputedRepresentationModel(TcrRepresentationModel):
     def __init__(self, model: TcrRepresentationModel) -> None:
         self._model = model
-        self._test_data_representations_cache = self._precompute_test_data_representations()
+        self._representation_cache_path = CACHE_DIR/f"{model.name}_rep_cache.pkl"
+        self._test_data_representations_cache = self._get_test_data_representations()
+    
+    def _get_test_data_representations(self) -> Dict[Tuple[Optional[str]], FloatTensor]:
+        if not self._representation_cache_path.is_file():
+            representations = self._precompute_test_data_representations()
+            self._save_cache(representations)
+            return representations
+        
+        with open(self._representation_cache_path, "rb") as f:
+            return pickle.load(f)
 
     def _precompute_test_data_representations(self) -> Dict[Tuple[Optional[str]], FloatTensor]:
         print(f"Precomputing {self._model.name} representations...")
@@ -31,6 +44,10 @@ class PrecomputedRepresentationModel(TcrRepresentationModel):
             representations_cache[tcr_identifier] = tcr_representations[idx]
         
         return representations_cache
+    
+    def _save_cache(self, representations) -> None:
+        with open(self._representation_cache_path, "wb") as f:
+            pickle.dump(representations, f)
 
     @property
     def name(self) -> str:

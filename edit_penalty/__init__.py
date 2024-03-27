c3a8df6a456b00e0ed16be7920b0f4a6cfeb19d5
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-03-27 14:00:14 +0000

    Add implementation edit penalty analysis

---
+++ edit_penalty/__init__.py
@@ -0,0 +1,2 @@
+from .edit_penalty_collection import EditPenaltyCollection
+from .edit_generator import get_all_tcr_variants
---
+++ edit_penalty_analysis.py
@@ -0,0 +1,62 @@
+from cached_representation_model import CachedRepresentationModel
+from datetime import datetime
+import edit_penalty
+from edit_penalty import EditPenaltyCollection
+import logging
+import pandas as pd
+from paths import DATA_DIR, RESULTS_DIR
+from sceptr import variant
+
+
+BACKGROUND_DATA = pd.read_csv(DATA_DIR/"preprocessed"/"tanno"/"test.csv")
+
+MODELS = (
+    CachedRepresentationModel(variant.ab_sceptr()),
+)
+
+
+current_time = datetime.now().isoformat()
+logging.basicConfig(filename=f"log_{current_time}.txt", level=logging.INFO)
+
+
+def main():
+    epc_per_model = {
+        model.name: explore_edit_penalties(model) for model in MODELS
+    }
+
+    for model_name, edit_penalty_collection in epc_per_model.items():
+        model_dir = RESULTS_DIR/model_name
+        model_dir.mkdir(exist_ok=True)
+        edit_penalty_collection.save(model_dir/"epc_state_dict.pkl")
+    
+    for model in MODELS:
+        if isinstance(model, CachedRepresentationModel):
+            model.save_cache()
+
+
+def explore_edit_penalties(model) -> EditPenaltyCollection:
+    print(f"Exploring edit penalties for {model.name}...")
+
+    edit_penalty_collection = EditPenaltyCollection()
+    num_tcrs_processed = 0
+
+    while not edit_penalty_collection.has_sufficient_coverage():
+        tcr = BACKGROUND_DATA.sample(n=1)
+        tcr_variants = edit_penalty.get_all_tcr_variants(tcr)
+        distances = model.calc_cdist_matrix(tcr, tcr_variants).squeeze()
+        edits_and_resulting_distances = zip(tcr_variants["edit"], distances)
+
+        for edit, distance in edits_and_resulting_distances:
+            edit_penalty_collection.update_edit_record(edit, distance)
+        
+        num_tcrs_processed += 1
+
+        if (num_tcrs_processed % 10) == 0:
+            print(f"{num_tcrs_processed} TCRs processed...")
+            edit_penalty_collection.print_current_estimation_coverage()
+    
+    return edit_penalty_collection
+
+
+if __name__ == "__main__":
+    main()

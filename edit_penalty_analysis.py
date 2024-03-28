e22f24834183add9850731c0b9b7a2211a0bcf51
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-03-28 18:40:17 +0000

    Add classic variant to models that we do edit penalty analysis on

--- edit_penalty_analysis.py
+++ edit_penalty_analysis.py
@@ -12,8 +12,9 @@ from sceptr import variant
 BACKGROUND_DATA = pd.read_csv(DATA_DIR/"preprocessed"/"tanno"/"test.csv")
 
 MODELS = (
-    variant.ab_sceptr(),
-    TcrBert(),
+    # variant.ab_sceptr(),
+    variant.ab_sceptr_classic(),
+    # TcrBert(),
 )
 
 
